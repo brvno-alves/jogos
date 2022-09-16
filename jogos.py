031977f20c5f0c6935ace71dc840814687c81ed1
Bruno R C Alves <bruno-rca@hotmail.com>
2022-09-16 14:58:00 -0300

    commit inicial

---
+++ jogos.py
@@ -0,0 +1,23 @@
+
+import forca
+import adivinhacao
+
+def escolha_o_jogo():
+
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("(1) Forca (2) Adivinhação")
+
+    jogo = int(input("Qual jogo? "))
+
+    if jogo == 1:
+        print("Jogando forca")
+        forca.jogar()
+    elif jogo == 2:
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+
+if escolha_o_jogo == "__main__":
+    escolha_o_jogo()
